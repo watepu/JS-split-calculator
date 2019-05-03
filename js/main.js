3c8619abcab951395a1e7ca0ca0601d6b6ae52a6
watepu <40508321+watepu@users.noreply.github.com>
2019-05-03 09:32:01 +0900

    jsで要素を取得

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+  const price = document.getElementById('price');
+  const num = document.getElementById('num');
+  const unit = document.getElementById('unit');
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const reset = document.getElementById('reset');
+
+  btn.addEvebtListener('click', function(){
+
+  });
+
+}
