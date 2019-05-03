4637cf0eb3500ec20086c25444cd3856abf0574f
watepu <40508321+watepu@users.noreply.github.com>
2019-05-03 10:55:00 +0900

    割り勘の計算式を作成

--- js/main.js
+++ js/main.js
@@ -8,7 +8,23 @@
   const result = document.getElementById('result');
   const reset = document.getElementById('reset');
 
-  btn.addEvebtListener('click', function(){
+  btn.addEventListener('click', function(){
+    const payLess = Math.floor(price.value / num.value /unit.value) * unit.value;
+    const short = price.value - (payLess * num.value);
+    const payMore = Math.ceil(price.value / num.value /unit.value) * unit.value;
+    const over = Math.abs(price.value - (payMore * num.value));
+    const str =
+    '一人' + payLess + '円だと' + short + '円足りません。　' +
+    '一人' + payMore + '円だと' + over + '円余ります。'
+
+    // payLess = Math.floor(price.value / num.value /unit.value) * unit.value;
+    // short = price.value - (payLess * num.value);
+    // payMore = Math.ceil(price.value / num.value /unit.value) * unit.value;
+    // over = Math.abs(price.value - (payMore * num.value));
+    // str =
+    // '一人' + payLess + '円だと' + short + '円足りません。　' +
+    // '一人' + payMore + '円だと' + over + '円余ります。'
+    result.textContent = str;
 
   });
 
