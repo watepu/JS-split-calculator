d5b13e17305f09b81b74af5d6d6189b99392dbd6
watepu <40508321+watepu@users.noreply.github.com>
2019-05-08 21:56:58 +0900

    計算ボタンを押すと、もう一度計算するが表示されるよう設定

--- js/main.js
+++ js/main.js
@@ -38,14 +38,8 @@
       '一人' + payMore + '円だと' + over + '円余ります。'
     }
 
-    // payLess = Math.floor(price.value / num.value /unit.value) * unit.value;
-    // short = price.value - (payLess * num.value);
-    // payMore = Math.ceil(price.value / num.value /unit.value) * unit.value;
-    // over = Math.abs(price.value - (payMore * num.value));
-    // str =
-    // '一人' + payLess + '円だと' + short + '円足りません。　' +
-    // '一人' + payMore + '円だと' + over + '円余ります。'
     result.textContent = str;
+    reset.classList.remove('hidden');
   });
 
   price.addEventListener('keyup', checkInput)
